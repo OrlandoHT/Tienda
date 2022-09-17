36758b5f679f700ef3ea28562d0baa1ba02ed915
Orlando Torres <orla88chido@hotmail.com>
2022-09-17 15:09:40 -0500

    Se modifico la clase Conexion

--- AccesoDatosTienda/Conexion.cs
+++ AccesoDatosTienda/Conexion.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data;
 using MySql.Data.MySqlClient;
 
 namespace AccesoDatosTienda
@@ -22,19 +17,6 @@ namespace AccesoDatosTienda
 
             _conn = new MySqlConnection(cadenaConexion.ToString());
         }
-        public void EjecutarConsulta(string consulta)
-        {
-            _conn.Open();
-            var command = new MySqlCommand(consulta, _conn);
-            command.ExecuteNonQuery();
-            _conn.Close();
-        }
-        public DataSet ObtenerDatos(string consulta, string tabla)
-        {
-            var ds = new DataSet();
-            MySqlDataAdapter da = new MySqlDataAdapter(consulta, _conn);
-            da.Fill(ds, tabla);
-            return ds;
-        }
+
     }
 }
